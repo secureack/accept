f68feffc3778a3e0b04d9129c9a2534df6e192e4
Simon Phillips <192426620+sack-simonphillips@users.noreply.github.com>
2025-02-18 12:04:56 +0000

    Add tracing capability to output processing for enhanced debugging

--- classes/output.py
+++ classes/output.py
@@ -1,4 +1,4 @@
-import logging
+import inspect
 import time
 
 from core import globalSettings, globalLogger
@@ -9,13 +9,22 @@ class output(base.base):
     def __init__(self,**kwargs):
         self.id = kwargs.get("id")
         self.logger = globalLogger.getLogger(__name__,kwargs.get("log_level",globalSettings.args.log_level))
+        self.trace = kwargs.get("trace",False)
         super().__init__(**kwargs)
 
     def processHandler(self,event):
         eventStartTime = time.perf_counter_ns()
+        if self.trace:
+            trace = []
+            for stack in inspect.stack():
+                try:
+                    if stack.function == "processHandler":
+                        trace.append(stack.frame.f_locals.get("self").id)
+                except:
+                    pass
+            self.logger.log(10,"Event Trace", { "trace" : trace }, extra={ "source" : "output", "type" : "trace" })
         self.process(event)
         self.updateProcessStats(eventStartTime)
         
     def process(self,event):
         pass
-        
